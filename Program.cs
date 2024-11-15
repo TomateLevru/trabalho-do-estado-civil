6bd58f3ecb84d1785dd633b4bc5533a95237543f
TomateLevru <podlmww@gmail.com>
2024-11-15 13:07:38 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class idadecivil
+{
+    public static void Main(string[] args)
+    {
+    int idade = 25;
+    bool isCasado = true;
+
+        if (idade < 18)
+        {
+            Console.WriteLine("é menor de idade");
+        }
+        else if  (idade >= 18 && idade < 65 && isCasado)
+        {
+            Console.WriteLine("é casado e é adulto");
+        }
+        else if (idade >= 18 && !isCasado)
+        {
+            Console.WriteLine("é solteiro e é adulto");
+        }
+        else
+        {
+            Console.WriteLine("é um senhor");
+        }  
+    }
+}
